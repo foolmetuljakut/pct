977bd427cf6779c6229b79a424df322d28ec6354
foolmetuljakut@TPE15G2 <foolmetuljakut@gmail.com>
2024-02-02 16:58:28 +0100

    bugfix + added selfcompiling instructions

--- comp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-echo "building pmt"
-
-g++ -o pmt pmt.cpp structure.hpp structure.cpp exec.hpp exec.cpp -fdiagnostics-color=always -Werror -Wfatal-errors -g -DBOOST_BIND_GLOBAL_PLACEHOLDERS #-lboost_program_options
